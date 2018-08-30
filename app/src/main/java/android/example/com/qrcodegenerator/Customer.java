df62811744ff79d125f65be7b556cfa5cf37b880
Deboprity.Das <pritydas001@gmail.com>
2018-08-30 17:19:49 -0500

    Scanning QR Code by Camera

---
+++ app/src/main/java/android/example/com/qrcodegenerator/Customer.java
@@ -0,0 +1,32 @@
+package android.example.com.qrcodegenerator;
+
+public class Customer {
+
+    private String name;
+    private int age;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- app/src/main/java/android/example/com/qrcodegenerator/MainActivity.java
+++ app/src/main/java/android/example/com/qrcodegenerator/MainActivity.java
@@ -1,75 +1,47 @@
 package android.example.com.qrcodegenerator;
 
-import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.google.gson.Gson;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
-    EditText editTextName, editTextAge, editTextPhone;
-    Button generateButton;
-    ImageView qrCode;
+    Button codeGenerator, codeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(TAG, "onCreate: started");
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextName = findViewById(R.id.edit_text_name);
-        editTextAge = findViewById(R.id.edit_text_age);
-        editTextPhone = findViewById(R.id.edit_text_phone);
-        generateButton = findViewById(R.id.qrGeneratorBtn);
-        qrCode = findViewById(R.id.qrImg);
+        codeGenerator = findViewById(R.id.generate_code);
+        codeScanner = findViewById(R.id.scan_code);
 
-        generateButton.setOnClickListener(new View.OnClickListener() {
+        codeGenerator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String textName = editTextName.getText().toString().trim();
-                String textAge = editTextAge.getText().toString().trim();
-                String textPhone = editTextPhone.getText().toString().trim();
-
-                if(TextUtils.isEmpty(textName) || TextUtils.isEmpty(textAge) || TextUtils.isEmpty(textPhone)){
-                    Toast.makeText(getBaseContext(), "Enter all the details", Toast.LENGTH_SHORT).show();
-                }
-
-                Customer customer = new Customer();
-                customer.setName(textName);
-                customer.setAge(Integer.parseInt(textAge));
-                customer.setPhone(textPhone);
-
-                Gson gson = new Gson();
-                String jsonString = gson.toJson(customer);
-
-                Log.d(TAG, "onClick: jsonString :: "+jsonString);
-
-                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-                try {
-                    BitMatrix bitMatrix = multiFormatWriter.encode(jsonString, BarcodeFormat.QR_CODE, 250, 250);
-                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-                    qrCode.setImageBitmap(bitmap);
-                    qrCode.setVisibility(View.VISIBLE);
-                    findViewById(R.id.information).setVisibility(View.GONE);
-                } catch (WriterException e) {
-                    e.printStackTrace();
-                }
+                Log.d(TAG, "onClick of codeGenerator");
+                Intent codeGeneratorIntent = new Intent(getApplicationContext(), CodeGeneratorActivity.class);
+                startActivity(codeGeneratorIntent);
             }
         });
+
+        codeScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick of codeScanner");
+                Intent codeScannerIntent = new Intent(getApplicationContext(), CodeScannerActivity.class);
+                startActivity(codeScannerIntent);
+            }
+        });
+
+        Log.d(TAG, "onCreate: ended");
+
     }
 }
